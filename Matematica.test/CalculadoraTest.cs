cc74d8bfd822bf0f8855dcae9f250182834f4722
Alberto Cadena <cadenaalberto@hotmail.com>
2020-12-18 14:17:17 -0500

    Add project files.

---
+++ Matematica.test/CalculadoraTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Matematica.test
+{
+    [TestClass]
+    public class CalculadoraTest
+    {
+        [TestMethod]
+        public void SumaDeDosNumeros()
+        {
+        }
+    }
+}
